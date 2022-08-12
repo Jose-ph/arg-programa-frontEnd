bd7c81c554e03f72c441bc432d1b2c768e321dd0
Jose <jose_esteban_basualdo@hotmail.com>
2022-08-12 17:16:54 -0300

    adds proyects components
    the name of the component has a typo

--- portfolio-front/src/app/app.module.ts
+++ portfolio-front/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { SkillsComponent } from './components/skills/skills.component';
     NavbarComponent,
     ExperienceComponent,
     EducationComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectsComponent
   ],
   imports: [
     BrowserModule,
