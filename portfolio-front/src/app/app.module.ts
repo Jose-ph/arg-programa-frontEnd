df176d984f088ee1d44824350fe076f28d7bde0c
Jose <jose_esteban_basualdo@hotmail.com>
2022-08-12 10:19:17 -0300

    adds about component

--- portfolio-front/src/app/app.module.ts
+++ portfolio-front/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoProgComponent } from './components/logo-prog/logo-prog.component';
 import { SocialComponent } from './components/social/social.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroComponent } from './components/hero/hero.component';
     HeaderComponent,
     LogoProgComponent,
     SocialComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
