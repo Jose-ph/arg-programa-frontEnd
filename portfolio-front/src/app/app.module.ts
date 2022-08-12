8375627c025b1b669bb65dac1aef7f1e810bd61d
Jose <jose_esteban_basualdo@hotmail.com>
2022-08-11 23:27:49 -0300

    adds logo hero and social

--- portfolio-front/src/app/app.module.ts
+++ portfolio-front/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoProgComponent } from './components/logo-prog/logo-prog.component';
+import { SocialComponent } from './components/social/social.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoProgComponent
+    LogoProgComponent,
+    SocialComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
